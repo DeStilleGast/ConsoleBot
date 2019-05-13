b18ec0fc0da2274e24506ad82b82b309fc2af01d
CFTheMaster <hentaimasterpad@gmail.com>
2019-05-13 21:42:52 +0200

    add DB Settings to Settings Class

--- src/main/kotlin/com/consolebot/settings/Settings.kt
+++ src/main/kotlin/com/consolebot/settings/Settings.kt
@@ -4,4 +4,4 @@ package com.consolebot.settings
  * Created by DeStilleGast 13-5-2019
  */
 
-class Settings(var bottoken: String)
+class Settings(var bottoken: String, var DBSchema: String, var DBHost: String, var DBPort: Int, var DBUsername: String, var DBPassword: String)
