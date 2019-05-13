7f7f7ffe52c3cdc341312f6c8d1d10b5f5527ac0
DeStilleGast <DeStilleGast@hotmail.com>
2019-05-13 23:37:33 +0200

    Reformated Settings.kt om het wat leesbaarder te maken SettingsLoader.kt, alle lege argumenten verplaats naar Settings.kt als optionele optie, dit maakt de code wat netter

--- src/main/kotlin/com/consolebot/settings/Settings.kt
+++ src/main/kotlin/com/consolebot/settings/Settings.kt
@@ -4,4 +4,11 @@ package com.consolebot.settings
  * Created by DeStilleGast 13-5-2019
  */
 
-class Settings(var bottoken: String, var DBSchema: String, var DBHost: String, var DBPort: String, var DBUsername: String, var DBPassword: String, var ShardTotal: String)
+class Settings(
+    var bottoken: String = "",
+    var DBSchema: String = "",
+    var DBHost: String = "",
+    var DBPort: String = "",
+    var DBUsername: String = "",
+    var DBPassword: String = "",
+    var ShardTotal: Int = 1)
