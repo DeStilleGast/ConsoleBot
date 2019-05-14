45caba08d7b1b950e5da3bc5d8d01989929ca0db
DeStilleGast <DeStilleGast@hotmail.com>
2019-05-14 16:43:08 +0200

    build, applied simple logging framework (get rid of annoying notification and the logging works now) Database connector has been tested and work

--- src/main/kotlin/com/consolebot/Main.kt
+++ src/main/kotlin/com/consolebot/Main.kt
@@ -2,6 +2,7 @@ package com.consolebot
 
 import com.consolebot.commands.BaseApplication
 import com.consolebot.commands.CommandManager
+import com.consolebot.database.DatabaseWrapper
 import com.consolebot.database.schema.Guilds
 import com.consolebot.extensions.asyncTransaction
 import com.consolebot.settings.Settings
@@ -13,10 +14,13 @@ import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.JDABuilder
 import net.dv8tion.jda.core.events.Event
 import net.dv8tion.jda.core.events.ReadyEvent
+import net.dv8tion.jda.core.events.guild.GuildJoinEvent
+import net.dv8tion.jda.core.events.guild.GuildLeaveEvent
 import net.dv8tion.jda.core.hooks.EventListener
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.reflections8.Reflections
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -42,8 +46,14 @@ class Main(botSettings: Settings) : EventListener {
     private val commandManager: CommandManager = CommandManager()
 
     init {
+        println("LOGGER.isErrorEnabled = ${LOGGER.isErrorEnabled}")
+        println("LOGGER.isWarnEnabled = ${LOGGER.isWarnEnabled}")
+        println("LOGGER.isInfoEnabled = ${LOGGER.isInfoEnabled}")
+        println("LOGGER.isDebugEnabled = ${LOGGER.isDebugEnabled}")
+        println("LOGGER.isTraceEnabled = ${LOGGER.isTraceEnabled}")
+
         Database.connect(
-            "jdbc:mysql://${botSettings.DBHost}:${botSettings.DBPort}/${botSettings.DBSchema}",
+            "jdbc:mysql://${botSettings.DBHost}:${botSettings.DBPort}/${botSettings.DBSchema}?autoReconnect=true&maxReconnects=5&serverTimezone=${TimeZone.getDefault().id}",
             "com.mysql.cj.jdbc.Driver",
             botSettings.DBUsername,
             botSettings.DBPassword
@@ -54,10 +64,13 @@ class Main(botSettings: Settings) : EventListener {
             )
         }.execute()
 
+
         findAndRegisterCommands()
         build(0,
             botSettings.ShardTotal - 1,
             botSettings.ShardTotal, botSettings.bottoken)
+
+        Main.LOGGER.error("TEST")
     }
 
     fun build(token: String){
@@ -90,13 +103,18 @@ class Main(botSettings: Settings) : EventListener {
         if (event is ReadyEvent) {
             println("Bot is ready !")
             println("Bot is in ${event.guildTotalCount} guilds with ${event.guildAvailableCount} available")
+        }else if(event is GuildJoinEvent){
+            DatabaseWrapper.newGuild(event.guild)
+            LOGGER.info("Joined ${event.guild.name}") // quick logging
+        }else if(event is GuildLeaveEvent){
+            LOGGER.info("Left ${event.guild.name}") // quick logging
         }
     }
 
     /**
      * Attempt and find commands in commands implementations (and subpackages)
      */
-    fun findAndRegisterCommands() {
+    private fun findAndRegisterCommands() {
         val reflections = Reflections("com.consolebot.commands.impl")
 
         val subTypes = reflections.getSubTypesOf(BaseApplication::class.java)
